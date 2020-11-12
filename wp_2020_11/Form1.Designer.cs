72d54da01486c824deb2c8637623eab24010a7d8
peter2330 <a0970690012@gmail.com>
2020-11-12 17:38:19 +0800

    FINAL

--- wp_2020_11/Form1.Designer.cs
+++ wp_2020_11/Form1.Designer.cs
@@ -28,56 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_go = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btn_go
+            // 
+            this.btn_go.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_go.Location = new System.Drawing.Point(114, 32);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(217, 85);
+            this.btn_go.TabIndex = 0;
+            this.btn_go.Text = "P1請按鈕";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(114, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(470, 161);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(233, 149);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(297, 28);
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(476, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 85);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 76);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "P2請按鈕";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(142, 144);
+            this.richTextBox1.Location = new System.Drawing.Point(114, 353);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(206, 198);
-            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Size = new System.Drawing.Size(224, 49);
+            this.richTextBox1.TabIndex = 10;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(364, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 55);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "V.S";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(463, 144);
+            this.richTextBox2.Location = new System.Drawing.Point(470, 353);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(204, 198);
-            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Size = new System.Drawing.Size(233, 49);
+            this.richTextBox2.TabIndex = 12;
             this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(364, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 48);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "歸零";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_go);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- wp_2020_11/Form1.cs
+++ wp_2020_11/Form1.cs
@@ -1,25 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using wp_2020_11.Properties;
 
 namespace wp_2020_11
 {
     public partial class Form1 : Form
     {
+        List<Image> list = new List<Image>();
+        int a , b ;
+        int player1 = 0, player2 = 0;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list.Add(Resources._2);
+            list.Add(Resources._3);
+            list.Add(Resources._4);
+            list.Add(Resources._6);
+            list.Add(Resources._5);
+            list.Add(Resources._7);
+            list.Add(Resources._8);
+            list.Add(Resources._9);
+            list.Add(Resources._10);
+            list.Add(Resources._11);
+            list.Add(Resources._12);
+            list.Add(Resources._13);
+            list.Add(Resources._14);
 
+
+        }
         private void button1_Click(object sender, EventArgs e)
+        {
+            int index = new Random().Next(13);//模擬隨機產生 一個值;
+            pictureBox1.Image = list[index];
+            a = index+1;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int index2 = new Random().Next(13);//模擬隨機產生 一個值;
+            pictureBox2.Image = list[index2];
+            b = index2+1;
+            
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+            if (a > b)
+            {
+                MessageBox.Show($"{a}>{b}", "P1贏了", MessageBoxButtons.OK);
+                player1 = player1 + 1;
+            }
+            else if (a < b)
+            {
+                MessageBox.Show($"{b}>{a}", "P2贏了", MessageBoxButtons.OK);
+                player2 = player2 + 1;
+            }
+            else if (a == b)
+            {
+                MessageBox.Show($"{b}={a}", "平手", MessageBoxButtons.OK);
+            }
+            richTextBox1.Text = "player1贏的次數" + player1 ;
+            richTextBox2.Text = "player2贏的次數" + player2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player1 = 0;
+            player2 = 0;
+            richTextBox1.Text = "player1贏的次數" + player1;
+            richTextBox2.Text = "player2贏的次數" + player2;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
